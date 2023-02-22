0cb5c1a6b0e137367171003b80d064e6c28ad8a2
Eric Nelson <ernelson12o0@gmail.com>
2023-02-22 13:55:07 -0800

    Lab Complete

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(cb) {
+    return cb();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        
+    }
+}
